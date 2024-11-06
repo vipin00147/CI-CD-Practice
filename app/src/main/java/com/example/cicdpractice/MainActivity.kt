68d2b488c26a4df1888798900fb7a5c9958073f3
Vipin <vipin.k@hashtechy.com>
2024-11-06 18:04:25 +0530

    updated code

--- app/src/main/java/com/example/cicdpractice/MainActivity.kt
+++ app/src/main/java/com/example/cicdpractice/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             CICDPracticeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android Testing",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
